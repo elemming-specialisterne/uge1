1be2cf82a1550a2d5ecaf7c2ea28bf255a515897
elemming-specialisterne <emil.lemming@specialisterne.com>
2025-09-15 14:01:56 +0200

    added help

--- Lommeregner.py
+++ Lommeregner.py
@@ -1,34 +1,60 @@
 import math
 
+help =  " Skriv et simpelt regnestykke med mellemrum mellem alle dele af det for at udregne de \n " \
+        " programmet tager i mod: + for addition, - for substraction, * for multiplication, / for division, ^ for potens, og // eller sqrt for kvadratrod \n " \
+        " skriv q eller quit for at lukke progammmet"
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    if y==0:
+        return "no division by 0"
+    return x / y
+
+def pot(x, y):
+    return x ** y
+
+def sqt(x):
+    return math.sqrt(x)
+
 # Chosses how to calculate based on sign, and does the calculation.
-def calculate(x, y, sign):
+def calculate(sign, int):
     match sign:
         case "+":
-            return float(x) + float(y)
+            return add(float(int[0]), float(int[1]))
         case "-":
-            return float(x) - float(y)
+            return sub(float(int[0]), float(int[1]))
         case "*":
-            return float(x) * float(y)
+            return mul(float(int[0]), float(int[1]))
         case "/":
-            if y=="0":
-                return "no division by 0"
-            return float(x) / float(y)
+            return div(float(int[0]), float(int[1]))
         case "^":
-            return float(x) ** float(y)
+            return pot(float(int[0]), float(int[1]))
         case "sqrt" | "//":
-            return  math.sqrt(float(x)) 
+            return  sqt(float(int[0])) 
         case _:
             return "failed"
 
 def main():
     print("------Lommeregner------")
     while(True):
-        print("giv input:")
+        print("skriv input eller \"help\":")
         i = input()
-        if i == "q":
+        if i == "q" or i == "quit":
             break
+        if i == "help":
+            print(help)
+            continue
         i = i.split()
-        print(calculate(i[0], i[2] if len(i) > 2 else i[0], i[1]))
+        sign = i.pop(1)
+        print("= " + str(calculate(sign, i)))
     print("lukker lommeregner")
 
 main()
